c84804a0142f3ca7beee6daef2fac6e13551626a
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-02-27 14:43:43 +0600

    banner done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <div>
-      <header>Header</header>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Header;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,14 +10,40 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="banner">
-      {/* <img src={banner} alt="Banner Graphic" /> */}
-      <div className="banner-container">
-        <h1 className={`banner-text ${animate ? "animate" : ""}`}>
-          Animated Banner Text
-          <span>Ashikur</span>
-        </h1>
-        <p className="scroll-down">Scroll down to learn</p>
+    <div>
+      <div className="banner">
+        <nav className="nav-banner">
+          <div>
+            <h1>Company Logo</h1>
+          </div>
+          <div>
+            <ul className="navbar">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Our Team</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {/* <img src={banner} alt="Banner Graphic" /> */}
+        <div className="banner-container">
+          <h1 className={`banner-text ${animate ? "animate" : ""}`}>
+            Animated Banner Text
+            <span>Ashikur</span>
+          </h1>
+          <p className="scroll-down">Scroll down to learn</p>
+        </div>
       </div>
     </div>
   );
